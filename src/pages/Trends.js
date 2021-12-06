ad06e1c579dea11129e65dca08a030be2724cc74
wantyouring <pwc99@naver.com>
2021-12-07 01:47:10 +0900

    Trends page 레이아웃 작업

--- src/pages/Trends.js
+++ src/pages/Trends.js
@@ -1,5 +1,49 @@
+import {
+  Avatar,
+  Grid,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import React from 'react';
+import FaceIcon from '@mui/icons-material/Face';
+
+function generate(element) {
+  return [...Array(30).keys()].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    }),
+  );
+}
 
 export default function Trends() {
-  return <div>Trends</div>;
+  return (
+    <>
+      <List>
+        {generate(
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <FaceIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText
+              primary={
+                <Grid container>
+                  <Grid item md={8}>
+                    <Typography>이름</Typography>
+                  </Grid>
+                  <Grid item md={4}>
+                    <Typography>코인 가격</Typography>
+                  </Grid>
+                </Grid>
+              }
+            />
+          </ListItem>,
+        )}
+      </List>
+    </>
+  );
 }
