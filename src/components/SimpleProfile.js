5d76f495657ac12eeaaf29a3b0de74d433f2f617
wantyouring <pwc99@naver.com>
2021-12-07 01:00:34 +0900

    SimpleProfile 레이아웃 작업

--- src/components/SimpleProfile.js
+++ src/components/SimpleProfile.js
@@ -1,5 +1,28 @@
 import React from 'react';
+import { Grid, Typography } from '@mui/material';
+import FaceIcon from '@mui/icons-material/Face';
 
 export default function SimpleProfile() {
-  return <div>(simple profile component)</div>;
+  return (
+    <>
+      <Grid container>
+        <Grid item md={3}>
+          <FaceIcon />
+        </Grid>
+        <Grid item md={9}>
+          <Grid container>
+            <Grid item md={12}>
+              <Typography>name</Typography>
+            </Grid>
+            <Grid item md={12}>
+              <Typography>token price</Typography>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item md={12}>
+          <Typography>simple description</Typography>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
