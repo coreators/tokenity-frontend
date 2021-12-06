9e7b87edb411024d1d7f64c1fb2e7cd6c12b15ab
wantyouring <pwc99@naver.com>
2021-12-07 02:03:28 +0900

    Notification page 레이아웃 작업

--- src/pages/Notification.js
+++ src/pages/Notification.js
@@ -1,5 +1,28 @@
+import { Grid, List, ListItem, ListItemText, Typography } from '@mui/material';
 import React from 'react';
+import { generate } from '../helpers';
 
 export default function Notification() {
-  return <div>Notification</div>;
+  return (
+    <Grid container align="center">
+      <Grid item md={12}>
+        <Typography> Notification</Typography>
+      </Grid>
+      <Grid item md={12}>
+        <List>
+          {generate(
+            <ListItem>
+              <ListItemText
+                primary={
+                  <Typography>
+                    wantyouring 님이 회원님을 팔로우하기 시작했습니다.
+                  </Typography>
+                }
+              />
+            </ListItem>,
+          )}
+        </List>
+      </Grid>
+    </Grid>
+  );
 }
