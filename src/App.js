0606e3507a175b5c28416a3dc5c37de5843ed766
Taeha Hong <xogk39@gmail.com>
2021-12-09 00:13:43 +0900

    Split App.js in index.js

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {
+  Routes,
+  Route,
+  Navigate,
+  Outlet,
+} from 'react-router-dom';
+import styled from 'styled-components';
+import { Grid } from '@mui/material';
+
+import Main from './pages/Main';
+import Notification from './pages/Notification';
+import Login from './pages/Login';
+import Trends from './pages/Trends';
+import Wallet from './pages/Wallet';
+import HeaderBar from './components/HeaderBar';
+import LeftBar from './components/LeftBar';
+import RightBar from './components/RightBar';
+import Profile from './pages/Profile';
+
+const Container = styled.div`
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 70px;
+`;
+const App = () => {
+  return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/main"
+          element={
+            <>
+              <HeaderBar />
+              <Container>
+                <Grid container>
+                  <LeftBar />
+                  <Grid item md={6}>
+                    <Outlet />
+                  </Grid>
+                  <RightBar />
+                </Grid>
+              </Container>
+            </>
+          }
+        >
+          <Route path="/main" element={<Main />} />
+          <Route path="/main/profile" element={<Profile />} />
+          <Route path="/main/trends" element={<Trends />} />
+          <Route path="/main/wallet" element={<Wallet />} />
+          <Route path="/main/notification" element={<Notification />} />
+        </Route>
+        <Route path="/*" element={<Navigate replace to="/main" />} />
+      </Routes>
+  );
+};
+
+export default App;
