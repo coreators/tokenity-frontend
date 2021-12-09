c01b5a283f76816ac5045bf2dac09d113df9adcd
wantyouring <pwc99@naver.com>
2021-12-10 03:22:00 +0900

    profile 페이지 정리

--- src/dummyData.js
+++ src/dummyData.js
@@ -3,6 +3,7 @@ import img2 from './assets/images/cosmonaut2.jpeg';
 import img3 from './assets/images/cosmonaut3.jpeg';
 import img4 from './assets/images/cosmonaut4.jpeg';
 import img5 from './assets/images/cosmonaut5.png';
+import postImg1 from './assets/images/imgTest.jpeg';
 
 export const tokenNames = [
   'pulse',
@@ -20,6 +21,59 @@ export const tokenPrices = [
 ];
 
 export const avatars = [img1, img2, img3, img4, img5];
+export const avatar = img1;
+export const postImg = postImg1;
+// export const postImgs = [...Array(10).keys()].map(() => postImg1);
+export const postImgs = [
+  {
+    img: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+    title: 'Bed',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+    title: 'Books',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+    title: 'Sink',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+    title: 'Kitchen',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+    title: 'Blinds',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+    title: 'Chairs',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+    title: 'Laptop',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+    title: 'Doors',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+    title: 'Coffee',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+    title: 'Storage',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+    title: 'Candle',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+    title: 'Coffee table',
+  },
+];
 
 export const tokens = tokenNames.map((name, index) => ({
   name,
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,55 +1,201 @@
-import { Grid, Typography } from '@mui/material';
+import {
+  Avatar,
+  Divider,
+  Grid,
+  ImageList,
+  ImageListItem,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Tab,
+  Tabs,
+  Typography,
+} from '@mui/material';
 import React from 'react';
-import FaceIcon from '@mui/icons-material/Face';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { Box } from '@mui/system';
+import { avatar, postImg, postImgs, tokens } from '../dummyData';
 
 export default function Profile() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const renderDetail = () => {
+    switch (value) {
+      case 0: // POSTS
+        return (
+          <ImageList variant="masonry" cols={3} gap={8}>
+            {postImgs.map(({ img, title }) => (
+              <ImageListItem key={img}>
+                <img
+                  src={`${img}?w=248&fit=crop&auto=format`}
+                  alt={title}
+                  loading="lazy"
+                />
+              </ImageListItem>
+            ))}
+          </ImageList>
+        );
+      case 1: // CREATOR COIN
+        return (
+          <Grid container rowSpacing={3}>
+            <Grid item md={0.5}></Grid>
+            <Grid item md={11.5}>
+              <Typography variant="h5" style={{ fontWeight: 'bold' }}>
+                Holders of $cosmonaut coin
+              </Typography>
+            </Grid>
+            <Grid item md={1} />
+            <Grid item md={7}>
+              <Typography style={{ color: 'GrayText' }}>
+                User Name or PubKey
+              </Typography>
+            </Grid>
+            <Grid item md={4}>
+              <Typography style={{ color: 'GrayText' }}>Coins Held</Typography>
+            </Grid>
+            <Grid item md={12}>
+              <Divider variant="middle" />
+            </Grid>
+
+            <Grid item md={12}>
+              {tokens.map(({ name, price, avatar }, index) => (
+                <>
+                  <ListItem key={index}>
+                    <ListItemAvatar>
+                      <Avatar src={avatar} />
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary={
+                        <Grid container>
+                          <Grid item md={8}>
+                            <Typography>{name}</Typography>
+                          </Grid>
+                          <Grid item md={4}>
+                            <Typography>~${price}</Typography>
+                          </Grid>
+                        </Grid>
+                      }
+                    />
+                  </ListItem>
+                </>
+              ))}
+            </Grid>
+          </Grid>
+        );
+      case 2: // NFTS
+        return (
+          <Box
+            style={{
+              borderColor: '#d3d3d3',
+              border: 'solid 1px',
+              padding: '20px',
+              borderRadius: '10px',
+            }}
+          >
+            <Grid container>
+              <Grid item md={1}>
+                <Avatar src={avatar} />
+              </Grid>
+              <Grid item md={11}>
+                <Grid container rowSpacing={1}>
+                  <Grid item md={12} />
+                  <Grid item md={12}>
+                    <Typography style={{ fontWeight: 'bold' }}>
+                      cosmonaut
+                    </Typography>
+                  </Grid>
+                  <Grid item md={12}>
+                    Hi, My new NFT~
+                  </Grid>
+                  <Grid item md={12}>
+                    <Typography style={{ color: 'blue' }}>
+                      #fire #100 #nft
+                    </Typography>
+                  </Grid>
+                  <Grid item md={12}>
+                    <img
+                      src={postImg}
+                      style={{ width: '100%', borderRadius: '20px' }}
+                    />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Box>
+        );
+    }
+  };
+
   return (
     <>
-      <Grid container>
-        <Grid item md={6}>
-          <FaceIcon />
+      <Grid container rowSpacing={4}>
+        <Grid item md={5} align="center">
+          <Avatar src={avatar} sx={{ width: 150, height: 150 }} />
         </Grid>
-        <Grid item md={6}>
-          <Grid container>
+        <Grid item md={7}>
+          <Grid container rowSpacing={2}>
             <Grid item md={12}>
-              <Typography>이름</Typography>
+              <Typography variant="h4">cosmonaut</Typography>
             </Grid>
             <Grid item md={12}>
-              <Typography>Coin Price</Typography>
+              <Grid container rowSpacing={1}>
+                <Grid item md={4}>
+                  <Typography style={{ fontWeight: 'bold' }}>
+                    Coin Price
+                  </Typography>
+                </Grid>
+                <Grid item md={8}>
+                  <Typography style={{ fontWeight: 'bold' }}>
+                    ~$128.85
+                  </Typography>
+                </Grid>
+                <Grid item md={4}>
+                  <Typography style={{ fontWeight: 'bold' }}>PubKey</Typography>
+                </Grid>
+                <Grid item md={5}>
+                  <Typography style={{ fontWeight: 'bold' }}>
+                    BC1YLiFNARS...
+                  </Typography>
+                </Grid>
+                <Grid item md={3}>
+                  <ContentCopyIcon />
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item md={12}>
-              <Typography>지갑 주소</Typography>
+              <Grid container rowSpacing={1}>
+                <Grid item md={4}>
+                  <Typography>164 posts</Typography>
+                </Grid>
+                <Grid item md={4}>
+                  <Typography>742 follwers</Typography>
+                </Grid>
+                <Grid item md={4}>
+                  <Typography>76 following</Typography>
+                </Grid>
+              </Grid>
             </Grid>
             <Grid item md={12}>
-              <Typography>게시물 14 팔로워 195204 팔로우 76</Typography>
-            </Grid>
-            <Grid item md={12}>
-              <Typography>나에 대한 소개 글</Typography>
+              <Typography>Hello~ I`m cosmonaut. </Typography>
+              <Typography>Nice to meet you!</Typography>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item md={4}>
-          <Typography>Posts</Typography>
+        <Grid item md={12}>
+          <Box>
+            <Tabs value={value} onChange={handleChange}>
+              <Tab label="Posts" />
+              <Tab label="creator coin" />
+              <Tab label="NFTs" />
+            </Tabs>
+          </Box>
         </Grid>
-        <Grid item md={4}>
-          <Typography>creator coin</Typography>
-        </Grid>
-        <Grid item md={4}>
-          <Typography>NFTs</Typography>
-        </Grid>
-        <Grid container>
-          <Grid item md={6}>
-            <Typography>Posts1</Typography>
-          </Grid>
-          <Grid item md={6}>
-            <Typography>Posts2</Typography>
-          </Grid>
-          <Grid item md={6}>
-            <Typography>Posts3</Typography>
-          </Grid>
-          <Grid item md={6}>
-            <Typography>Posts4</Typography>
-          </Grid>
+        <Grid item md={12}>
+          {renderDetail()}
         </Grid>
       </Grid>
     </>
