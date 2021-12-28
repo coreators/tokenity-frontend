bcfc337273c4ae8bb7c43746e9450a59f2d498db
Taeha Hong <xogk39@gmail.com>
2021-12-28 21:18:24 +0900

    Complete sidebar button clicked

--- src/components/Side/LeftBar.js
+++ src/components/Side/LeftBar.js
@@ -1,7 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Grid } from '@mui/material';
-// import styled from 'styled-components';
 
 import Wallet from '../Wallet';
 import SimpleProfile from '../SimpleProfile';
@@ -10,35 +9,51 @@ import SidebarLink from './SidebarLink';
 // const Container = styled.span`
 //   position: fixed;
 //   height: 100%;
-//   width: 200px; 
+//   width: 200px;
 //   background-color: yellow;
 //   border-bottom: 1px solid black;
 //   z-index: 99;
 // `;
 
+const linkArr = [
+  { text: 'Home', link: '/main' },
+  { text: 'Create', link: '/main' },
+  { text: 'Trends', link: '/main/trends' },
+  { text: 'Profile', link: '/main/profile' },
+  { text: 'Wallet', link: '/main/wallet' },
+  { text: 'Notification', link: '/main/notification' },
+];
+
 export default function LeftBar() {
   const { data } = useSelector((state) => state.account.account);
+  const [clicked, setClicked] = useState(0);
 
   return (
     // <Container>
-      <Grid item md={3}>
-        <Grid container>
-          <Grid item md={12}>
-            {data ? <SimpleProfile /> : <Wallet />}
-          </Grid>
-          <Grid item md={1} />
-          <Grid item md={11}>
-            <Grid container rowSpacing={3}>
-              <SidebarLink text={'Home'} link={'/main'} clicked />
-              <SidebarLink text={'Create'} link={'/main'} />
-              <SidebarLink text={'Trends'} link={'/main/trends'} />
-              <SidebarLink text={'Profile'} link={'/main/profile'} clicked />
-              <SidebarLink text={'Wallet'} link={'/main/wallet'} />
-              <SidebarLink text={'Notification'} link={'/main/notification'} />
-            </Grid>
+    <Grid item md={3}>
+      <Grid container>
+        <Grid item md={12}>
+          {data ? <SimpleProfile /> : <Wallet />}
+        </Grid>
+        <Grid item md={1} />
+        <Grid item md={11}>
+          <Grid container rowSpacing={3}>
+            {linkArr.map((data, i) => {
+              return (
+                <SidebarLink
+                  id={i}
+                  text={data.text}
+                  link={data.link}
+                  clicked={clicked}
+                  setClicked={setClicked}
+                  key={i}
+                />
+              );
+            })}
           </Grid>
         </Grid>
       </Grid>
+    </Grid>
     // </Container>
   );
 }
